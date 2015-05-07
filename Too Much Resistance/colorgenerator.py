09da538c871fb39a1e298874fa4eaaacb733277d
Pedon <sarporgul@gmail.com>
2015-05-06 21:26:42 -0400

    added color.txt

--- Too Much Resistance/colorgenerator.py
+++ Too Much Resistance/colorgenerator.py
@@ -1,6 +1,6 @@
 dict = {0 : 'Black',1 : 'Brown',2 : 'Red',3 : 'Orange',4 : 'Yellow',5 : 'Green',6 : 'Blue',7 : 'Violet',8 : 'Grey',9 :'White'}
 
-flag = input('Enter flag: ')
+flag = "ohmohmohmthatwasafunnyone"
 
 for chd in flag:
     ch = ord(chd)
