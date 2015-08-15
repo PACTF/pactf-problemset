7f41d3f461fddf4e084709742debcf23bd3de8ed
Sarp Orgul <sarporgul@gmail.com>
2015-08-14 20:27:29 -0700

    added files for problem whitespace

---
+++ programming/Whitespace/whitespacejava.java
@@ -0,0 +1,67 @@
+public class whitespace {	 	 	  
+	
+    public static	void	main(String[] args)	{   
+	
+     System.out.println("You	will	find the flag	when you	run");
+	
+     System.out.print("this program");	     
+	
+     System.out.println(obuscate());		  		 
+	
+    } 		 		  
+	
+    public static	String	obfuscate() {   	
+	
+     int	i='w'	, j='s' ,	k='p'	,	l='a'
+	
+      	     
+T	L
+S S S S S 0	1	1 0	1 0 0	1i
+T	L
+S S S S S 0	1	1	1 0 0	1	1s
+T	L
+S S S S S 0 0	1	1	1 0	1 0:
+T	L
+S S S S S 0 0	1 0 0 0 0 0
+T	L
+S S S S S 0	1	1	1 0	1 0 0t
+T	L
+S S S S S 0	1	1	1 0	1	1	1w
+T	L
+S S S S S 0	1	1 0	1	1	1	1o
+T	L
+S S S S S 0	1 0	1	1	1	1	1_
+T	L
+S S S S S 0	1	1	1 0 0 0 0p
+T	L
+S S S S S 0	1	1	1 0 0	1 0r
+T	L
+S S S S S 0	1	1 0	1	1	1	1o
+T	L
+S S S S S 0	1	1 0 0	1	1	1g
+T	L
+S S S S S 0	1	1	1 0 0	1 0r
+T	L
+S S S S S 0	1	1 0 0 0 0	1a
+T	L
+S S S S S 0	1	1 0	1	1 0	1m
+T	L
+S S S S S 0	1	1	1 0 0	1	1s
+T	L
+S S S S S 0	1 0	1	1	1	1	1_
+T	L
+S S S S S 0	1	1 0	1 0 0	1i
+T	L
+S S S S S 0	1	1 0	1	1	1 0n
+T	L
+S S S S S 0	1 0	1	1	1	1	1_
+T	L
+S S S S S 0	1	1 0	1	1	1	1o
+T	L
+S S S S S 0	1	1 0	1	1	1 0n
+T	L
+S S S S S 0	1	1 0 0	1 0	1e
+T	L
+S S L
+L
+L
