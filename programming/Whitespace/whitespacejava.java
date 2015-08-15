132596686262148404a9a78e637cef6927b0bd5c
Sarp Orgul <sarporgul@gmail.com>
2015-08-14 21:43:56 -0700

    finished whitespace java program

--- programming/Whitespace/whitespacejava.java
+++ programming/Whitespace/whitespacejava.java
@@ -6,62 +6,62 @@ public class whitespace {
 	
      System.out.print("this program");	     
 	
-     System.out.println(obuscate());		  		 
+     System.out.println(obfuscate());		  		 
 	
     } 		 		  
 	
     public static	String	obfuscate() {   	
 	
-     int	i='w'	, j='s' ,	k='p'	,	l='a'
+     int	i='w'	, j='s' ,	k='p'	,	l='a';
 	
-      	     
-T	L
-S S S S S 0	1	1 0	1 0 0	1i
-T	L
-S S S S S 0	1	1	1 0 0	1	1s
-T	L
-S S S S S 0 0	1	1	1 0	1 0:
-T	L
-S S S S S 0 0	1 0 0 0 0 0
-T	L
-S S S S S 0	1	1	1 0	1 0 0t
-T	L
-S S S S S 0	1	1	1 0	1	1	1w
-T	L
-S S S S S 0	1	1 0	1	1	1	1o
-T	L
-S S S S S 0	1 0	1	1	1	1	1_
-T	L
-S S S S S 0	1	1	1 0 0 0 0p
-T	L
-S S S S S 0	1	1	1 0 0	1 0r
-T	L
-S S S S S 0	1	1 0	1	1	1	1o
-T	L
-S S S S S 0	1	1 0 0	1	1	1g
-T	L
-S S S S S 0	1	1	1 0 0	1 0r
-T	L
-S S S S S 0	1	1 0 0 0 0	1a
-T	L
-S S S S S 0	1	1 0	1	1 0	1m
-T	L
-S S S S S 0	1	1	1 0 0	1	1s
-T	L
-S S S S S 0	1 0	1	1	1	1	1_
-T	L
-S S S S S 0	1	1 0	1 0 0	1i
-T	L
-S S S S S 0	1	1 0	1	1	1 0n
-T	L
-S S S S S 0	1 0	1	1	1	1	1_
-T	L
-S S S S S 0	1	1 0	1	1	1	1o
-T	L
-S S S S S 0	1	1 0	1	1	1 0n
-T	L
-S S S S S 0	1	1 0 0	1 0	1e
-T	L
-S S L
-L
-L
+     i=i^7; 	     
+	
+     j=j>>1;		 	  	
+	
+     l=l<<1;			  		
+	
+     i-=2; 			 	 
+	
+     j+=54; 	     
+	
+     l-=93;			 	  
+	
+     char[]	a={i,j,k,l};		 			
+	
+     String	b=new	String(a); 				
+	
+    } 	 					
+	
+}     			    
+	
+     			  	 
+	
+     		 				
+	
+     		  			
+	
+     			  	 
+	
+     		    	
+	
+     		 		 	
+	
+     			  		
+	
+     	 					
+	
+     		 	  	
+	
+     		 			 
+	
+     	 					
+	
+     		 				
+	
+     		 			 
+	
+     		  	 	
+	
+  
+
+
