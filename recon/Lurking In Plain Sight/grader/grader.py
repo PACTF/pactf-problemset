00ddba6a70e72ea0ba2982f44595ae1a45f5b342
Tony Zhaocheng Tan <tony@tonytan.io>
2015-08-18 20:46:33 +0800

    new problem

---
+++ recon/Lurking In Plain Sight/grader/grader.py
@@ -0,0 +1,5 @@
+    def grade(arg, key):
+	    if "i_found_a_header!" in key:
+	    	return True, "Correct. You found it."
+	    else:
+	        return False, "Incorrect. Are you looking in the right place?"
