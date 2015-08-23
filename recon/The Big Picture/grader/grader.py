0ab954c3bda4f4adf228960bafa9424191f108c4
Tony Zhaocheng Tan <tony@tonytan.io>
2015-08-23 20:34:19 +0800

    "Big Picture" problem

---
+++ recon/The Big Picture/grader/grader.py
@@ -0,0 +1,5 @@
+    def grade(arg, key):
+        if "s6D1lyLbH5HSwkqBdFAv" in key:
+            return True, "Correct!"
+        else:
+            return False, "Incorrect. Are you missing the big picture?"
