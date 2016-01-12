180962fbe3d80be7cdbdc546abebab92cfb00e50
Cameron Wong <cam@lancer>
2016-01-12 02:06:43 -0500

    add grader for rev/randomgame

---
+++ reversing/randomgame/grader.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+def grade(arg, key):
+    if 'PA{m1Nd_j00r_sE3Dz}' in key:
+        return True, "What's wrong with system time, anyway?"
+    else:
+        return False, "Try again!"
