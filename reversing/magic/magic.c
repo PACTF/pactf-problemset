91b5e466513aad40b74f795f92a42dbf7b29d163
Cameron Wong <cam@lancer>
2016-03-21 00:42:30 -0400

    add magic and concepts

--- reversing/magic/magic.c
+++ reversing/magic/magic.c
@@ -4,17 +4,11 @@ char a = 'k';
 
 void magic() {
   char *c;
-  // cHECk_uR_vAr5
-  long offs[13] = {
-    0x2E4, 0x3A8, 0x001, 0xA14, 0x15D8, 0x13D2,
-    0x1372, 0x1390, 0x1398,
-    0x139A, 0x1286, 0x1294, 0xE18
+  // pa{cHECk_uR_vAr5}
+  char msg[17] = {
+    'p','a','{', 'c','H','E','C','k','_','u','R','_','v','A','r','5','}'
   };
-  char msg[13];
-  for (int i = 0 ; i < 13 ; i++) {
-    long a = 0x400000 + offs[13];
-    c = (char *) a;
-  }
+  printf("The magic is happening at %p...\n", msg);
 }
 
 int main() {
