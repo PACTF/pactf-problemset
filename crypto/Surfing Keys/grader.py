67d4b8c9c5fdd7f89d772a9802a223523ec3338d
Reichenbachian <alex@reichenbach.org>
2016-03-29 19:42:53 -0400

    committed

--- crypto/Surfing Keys/grader.py
+++
@@ -1,5 +0,0 @@
-def grade(key, flag):
-    if flag == 'qwertyisfornoobsdvorakdrools':
-        return True, "You have found the way."
-    else:
-        return False, "Incorrect. Have you tried being more obscure?"
