4e8eabf57db0b1116c060fbef4de9302554795a8
Cameron Wong <cam@lancer>
2016-04-09 15:16:42 -0400

    MAKE OTP SOLVABLE

--- crypto/otp/generator.py
+++ crypto/otp/generator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import random, string
+import random, string, itertools
 
 m1 = "I wonder what the flag could be? It's not like you can crack this cipher!"
 m2 = "The flag, that you can't crack, is this: cR1b_dr4G_b01z_{%s}"
@@ -20,7 +20,11 @@ def hex_str(s):
 def generate(key):
     random.seed(key)
     m2_ = m2 % randword(len(m1)-len(m2)+2)
-    key = randword(len(m1))
+    key = itertools.cycle(randword(len(m1) // 2))
     m1_c = hex_str(ord(c1) ^ ord(c2) for c1, c2 in zip(m1, key))
     m2_c = hex_str(ord(c1) ^ ord(c2) for c1, c2 in zip(m2_, key))
-    return desc.format(m1_c, m2_c), "Don't drag your feet, welcome to my crib!"
+    return (
+        desc.format(m1_c, m2_c),
+        "Don't drag your feet, welcome to my crib! "
+        "Don't throw out your keys, just use them over and over again!"
+    )
