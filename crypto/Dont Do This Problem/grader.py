c06afa80b54395afabc3685b52d8e172e2f03607
Reichenbachian <alex@reichenbach.org>
2016-04-08 23:35:14 -0400

    Still fixing.

--- crypto/Dont Do This Problem/grader.py
+++
@@ -1,5 +0,0 @@
-    def grade(arg, key):
-        if "not the flag" in key:
-            return True, "Correct. But you really should not have done this problem! Why!?"
-        else:
-            return False, "Incorrect. Try this secretive problem again?"
