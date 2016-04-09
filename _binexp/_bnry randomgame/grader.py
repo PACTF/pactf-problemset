be7e0ce6fd424abb03439353924a551c0a997ede
Reichenbachian <alex@reichenbach.org>
2016-04-08 23:11:05 -0400

    Trying to fix. Sorry all\!

--- _binexp/_bnry randomgame/grader.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-def grade(arg, key):
-    if 'PA{m1Nd_j00r_sE3Dz}' in key:
-        return True, "What's wrong with system time, anyway?"
-    else:
-        return False, "Try again!"
