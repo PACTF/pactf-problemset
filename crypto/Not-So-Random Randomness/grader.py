10d74713a991a0edde756c815b1556a442f2cff8
Cameron Wong <cam@lancer>
2016-04-12 00:16:58 -0400

    fix NSRR grader

--- crypto/Not-So-Random Randomness/grader.py
+++ crypto/Not-So-Random Randomness/grader.py
@@ -1,4 +1,4 @@
 def grade(key, flag):
-    if "Aren't_Weak_Encryptions_Fun!?!?!?!?!" in flag:
+    if "D0n\'t_Always_TRusT_DaT_D3bian" in flag:
         return True, "Yay Debian!"
     return False, "Come on. Debian ain't that secure..."
