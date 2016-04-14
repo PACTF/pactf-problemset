3ee923c670bd79df4f7a28d8035bdb876071c0db
Reichenbachian <alex@reichenbach.org>
2016-04-13 20:30:09 -0400

    Finished sorcery

---
+++ binary/_sorcery/sorcery.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <cstdint>
+using namespace std;
+void flag(){
+		char start = '!';
+	char q = 'q';
+	char z = 'z';
+	char s = 's';
+	char a = 'a';
+	char x = 'x';
+	char w = 'w';
+	char c = 'c';
+	char e = 'e';
+	char f = 'f';
+	char d = 'd';
+	char v = 'v';
+	char r = 'r';
+	char b = 'b';
+	char t = 't';
+	char n = 'n';
+	char g = 'g';
+	char h = 'h';
+	char asdf = '{';
+	char y = 'y';
+	char m = 'm';
+	char j = 'j';
+	char u = 'u';
+	char k = 'k';
+	char i = 'i';
+	char l = 'l';
+	char o = 'o';
+	char p = 'p';
+	char one = '1';
+	char six = '6';
+	char two = '2';
+	char five = '5';
+	char qw = '3';
+	char re = '9';
+	char yt = '5';
+	char tr = '0';
+	char fdsa = '}';
+	char uy = '4';
+	char iu = '9';
+	char as = '2';
+	char sd = '8';
+	char df = '3';
+	char fg = '7';
+	char gh = '4';
+	char asdf2 = 'a';
+	char fdasa2 = 'f';
+	char fdasa = '_';
+	char asdf3 = '-';
+	char flag17 = *reinterpret_cast<char *>(&start-23);
+	char flag18 = *reinterpret_cast<char *>(&start-46);
+	char flag3 = *reinterpret_cast<char *>(&start-35);
+	char flag4 = *reinterpret_cast<char *>(&start-4);
+	char flag5 = *reinterpret_cast<char *>(&start-17);
+	char flag6 = *reinterpret_cast<char *>(&start-46);
+	char flag10 = *reinterpret_cast<char *>(&start-14);
+	char flag11 = *reinterpret_cast<char *>(&start-4);
+	char flag12 = *reinterpret_cast<char *>(&start-46);
+	char flag13 = *reinterpret_cast<char *>(&start-7);
+	char flag14 = *reinterpret_cast<char *>(&start-17);
+	char flag15 = *reinterpret_cast<char *>(&start-8);
+	char flag24 = *reinterpret_cast<char *>(&start-8);
+	char flag25 = *reinterpret_cast<char *>(&start-20);
+	char flag1 = *reinterpret_cast<char *>(&start-18);
+	char flag2 = *reinterpret_cast<char *>(&start-6);
+	char flag19 = *reinterpret_cast<char *>(&start-10);
+	char flag20 = *reinterpret_cast<char *>(&start-4);
+	char flag21 = *reinterpret_cast<char *>(&start-14);
+	char flag22 = *reinterpret_cast<char *>(&start-46);
+	char flag23 = *reinterpret_cast<char *>(&start-20);
+	char flag8 = *reinterpret_cast<char *>(&start-26);
+	char flag9 = *reinterpret_cast<char *>(&start-28);
+	char flag26 = *reinterpret_cast<char *>(&start-36);
+	char flag16 = *reinterpret_cast<char *>(&start-7);
+	char flag7 = *reinterpret_cast<char *>(&start-16);
+	cout << "test: " <<flag1<<flag2<<flag3<<flag4<<flag5<<flag6<<flag7<<flag8<<flag9<<flag10<<flag11<<flag12<<flag13<<flag14<<flag15<<flag16<<flag17<<flag18<<flag19<<flag20<<flag21<<flag22<<flag23<<flag24<<flag25<<flag26<<endl;
+}
+int main(int argc, char **argv) {
+	flag();
+	return 1;
+}
