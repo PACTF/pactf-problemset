90d32c8bd9b6a0d048f1d9e6b8ee5f5beb6897da
Cameron Wong <camtech075@gmail.com>
2016-04-16 04:00:35 -0400

    wtf

--- binary/GotDatObjC?/grader.py
+++
@@ -1,4 +0,0 @@
-def grade(team, flag):
-    if '{lovn_dis_ObjC' in flag:
-        return True, "Nice. It's gonna get harder, though!"
-    return False, "Bit harder when it's not in you architecture. Isn't it?"
--- crypto/You are a square./grader.py
+++
@@ -1,5 +0,0 @@
-def grade(key, flag):
-    if flag.lower() == 'flag is guess you’re not a square! xpsogpps' or flag.lower() == 'guess you’re not a square! xpsogpps' or flag.lower() == 'xpsogpps':
-        return True, "No square. No more."
-    else:
-        return False, "You are clearly a square."
