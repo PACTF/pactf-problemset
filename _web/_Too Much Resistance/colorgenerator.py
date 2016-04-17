9db852d15da3c178810a0b4c673dd793ec9bfe73
Yatharth Agarwal <yatharth999@gmail.com>
2016-04-17 12:42:56 -0400

    Reorganize problems

--- _web/_Too Much Resistance/colorgenerator.py
+++
@@ -1,7 +0,0 @@
-dict = {0 : 'Black',1 : 'Brown',2 : 'Red',3 : 'Orange',4 : 'Yellow',5 : 'Green',6 : 'Blue',7 : 'Violet',8 : 'Grey',9 :'White'}
-
-flag = "ohmohmohmthatwasafunnyone"
-
-for chd in flag:
-    ch = ord(chd)
-    print(dict[(ch-ch%100)/100] + ' ' + dict[(ch%100-ch%10)/10] + ' ' + dict[ch%10])
