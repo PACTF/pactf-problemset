2a6447bae5242aa281b85756d14a5e5c21a291d5
Yatharth Agarwal <yatharth999@gmail.com>
2016-04-17 14:12:49 -0400

    Fix Regex problems

--- binary/Regex 3/grader.py
+++ binary/Regex 3/grader.py
@@ -3,56 +3,42 @@
 __author__ = 'Yatharth Agarwal <yatharth999@gmail.com>'
 
 import re
-import random
+
+
+def check(flag):
+    POWER = 5
+    TEST_LIMIT = 3
+
+    pat = re.compile(flag)
+
+    upper = POWER ** (TEST_LIMIT - 1) + 3
+    powers = [POWER ** i for i in range(TEST_LIMIT)]
+    for times in range(1, upper):
+        s = 'x' * times
+        print(times, pat.fullmatch(s), times in powers)
+        if bool(pat.fullmatch(s)) != (times in powers):
+            return times
+
+    for exponent in range(5):
+        for delta in range(4):
+            times = POWER ** exponent + delta
+            s = 'x' * times
+            print(times, exponent, delta, pat.fullmatch(s), bool(delta))
+            if bool(pat.fullmatch(s)) == bool(delta):
+                return times
+
+    return -1
 
 
 def grade(key, flag):
-    power = 5
+    try:
+        fail = check(flag)
+    except Exception as err:
+        print(err)
+        return False, "Please try a different regex."
 
-    limit = 3
-    upper = power**(limit-1) + 1
-    powers = [power**i for i in range(limit)]
-    flag = flag
-
-    print("Powers")
-    print()
-    for i, power in enumerate(powers):
-        s = 'x' * power
-        print(re.match(flag, s) is not None, '\t', i)
-        if re.match(flag, s) is None:
-            return False, "You're missing at least one case."
-    print()
-    print()
-
-    print("Random")
-    print()
-    for i in range(20):
-        times = random.randint(1, upper)
-        s = 'x' * times
-        if times in powers:
-            print("Ski", '\t', i, times)
-            continue
-        print(re.match(flag, s) is None, '\t', i, times)
-        if  re.match(flag, s) is not None:
-            return False, "You're missing at least one case."
-
-    print()
-    print()
-
-    print("All", flush=True)
-    print()
-
-    for i in range(1, upper):
-        if i in powers:
-            continue
-        s = 'x' * i
-        if re.match(flag, s) is not None:
-            print(re.match(flag, s) is None, '\t', i, times, flush=True)
-            if re.match(flag, s) is not None:
-                return False, "You're missing at least one case."
-
-    print()
-    print()
-
-    print("Done")
-    return True, "You weild The Elegant Weapon for a More Civilized Age"
+    if fail < 0:
+        return True, "Though don’t actually use this."
+    else:
+        # print(fail)
+        return False, "You’re missing at least one case."
