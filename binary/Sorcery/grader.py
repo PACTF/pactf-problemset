076d5ad45b07f29490ebc0049c7d3f991f67d755
Reichenbachian <alex@reichenbach.org>
2016-04-19 19:16:47 -0400

    So much shame

--- binary/Sorcery/grader.py
+++ binary/Sorcery/grader.py
@@ -1,4 +1,4 @@
 def grade(arg, key):
-	if 'w0ah_go1ta_check_dat_mem' in key:
+	if '1_dun_ducked_up!' in key:
 		return True, "I was pointing to it the whole time!"
 	else: return False, 'That "Points" to the wrong answer.'
