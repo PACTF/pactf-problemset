6a5f1ff27447ad81a7bd254682109789d6e4e2e3
Yatharth Agarwal <yatharth999@gmail.com>
2016-04-24 23:04:26 -0400

    Fix Injection II flag

--- web/web Injection II/grader.py
+++ web/web Injection II/grader.py
@@ -1,4 +1,4 @@
 def grade(team, key):
-    if "bobby_little_tables" in key:
+    if "bobby_tables_little" in key:
         return True, "You'd think people'd stop trusting PHP..."
     return False, "Damn it, Phil..."
