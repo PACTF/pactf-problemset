68c3468e2ef7477fb77d7ec4308e465ee1e609df
Tony Zhaocheng Tan <tony@tonytan.io>
2016-04-24 21:17:14 -0400

    heartbroken

--- web/web Heartbroken/grader.py
+++ web/web Heartbroken/grader.py
@@ -1,8 +1,8 @@
     def grade(arg, key):
-	    if "4fV8cM2QfE2TJcHMxdSrvSye+ic/bQHlw+4Bm9nwls0=" in key:
+	    if "57Yh+k6TTFiz63os1nWhAP/g5X9UBWzcj7pl91beLbE=" in key:
                     return True, "Congratulations! It worked!"
-	    elif "DBZRU8OYyg0bvI0x2F+LL+QcYRLIKWLPyz92dwuaPDs=" in key:
-                    return False, "No, it didn't work. I need the hash of the private key, not the public key."
+	    elif "lxgHG27ibhPKlbUq0XgQHLGUXV+tQGzoDNeqZXTz46k=" in key:
+                    return False, "Use DER, not PEM, before hashing."
 	    elif "lv8t74hplkz79CIsCsBzDiaTfJF1skxv0rLhkV5Gw2A=" in key:
                     return False, "No, the hash looks something like this, but this is not it."
 	    else:
