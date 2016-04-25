8f4cabd7e3889b227a191147fad68c364ab8547c
Yatharth Agarwal <yatharth999@gmail.com>
2016-04-24 23:05:21 -0400

    Tighten grader

--- web/web Injection II/grader.py
+++ web/web Injection II/grader.py
@@ -1,4 +1,4 @@
 def grade(team, key):
-    if "bobby_tables_little" in key:
+    if "bobby_tables_little" == key.strip():
         return True, "You'd think people'd stop trusting PHP..."
     return False, "Damn it, Phil..."
